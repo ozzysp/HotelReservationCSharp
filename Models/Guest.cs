4342aa5a05a31255ab22782e034e468bc9135ae4
Ozzy Scuba <ozzysp@icloud.com>
2023-04-17 04:01:29 -0300

    Creates hotel classes

---
+++ Models/Guest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelReservationCSharp.Models
+{
+    public class Guest
+    {
+        public int Name { get; set; }
+        public int Surname { get; set; }
+    }
+}
