5bf3a2dc6e2bca47dc15f93052485077a04af499
Ozzy Scuba <ozzysp@icloud.com>
2023-04-17 04:17:39 -0300

    Creates hotel model

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using HotelReservationCSharp.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Guest> guests = new List<Guest>();
+
+Guest g1 = new Guest(name: "");
+Guest g2 = new Guest(name: "");
+Guest g3 = new Guest(name: "");
+Guest g4 = new Guest(name: "");
+
+guests.Add(g1);
+guests.Add(g2);
+guests.Add(g3);
+guests.Add(g4);
+
+Room room = new Room(typeRoom: "Premium", capacity: 3, valueDay: 30);
+
+Reservation reservation = new Reservation(reservationDays: 10);
+reservation.RegisterRoom(room);
+reservation.RegisterGuest(guest);
+
+Console.WriteLine($"Guests: {reservation.GetGuestsQuantity()}");
+Console.WriteLine($"Value per day: {reservation.CalcValueDay}");
+
